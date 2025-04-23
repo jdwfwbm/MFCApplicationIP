27eee8161c7ad90564abe510b6113397d0e4c1b0
jdwfwbm <ted0426@aver.com>
2025-04-23 11:40:53 +0900

    초기 커밋.

---
+++ MFCApplicationIP/MFCApplicationIPView.cpp
@@ -0,0 +1,145 @@
+﻿
+// MFCApplicationIPView.cpp: CMFCApplicationIPView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MFCApplicationIP.h"
+#endif
+
+#include "MFCApplicationIPDoc.h"
+#include "MFCApplicationIPView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplicationIPView
+
+IMPLEMENT_DYNCREATE(CMFCApplicationIPView, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplicationIPView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMFCApplicationIPView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplicationIPView 생성/소멸
+
+CMFCApplicationIPView::CMFCApplicationIPView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMFCApplicationIPView::~CMFCApplicationIPView()
+{
+}
+
+BOOL CMFCApplicationIPView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplicationIPView 그리기
+
+void CMFCApplicationIPView::OnDraw(CDC* pDC)
+{
+	CMFCApplicationIPDoc* pDoc = GetDocument(); // 도큐먼트 클래스 참조
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	int i, j;
+	unsigned char R, G, B;
+
+	for (i = 0; i < pDoc->m_height; i++) {
+		for (j = 0; j < pDoc->m_width; j++) {
+			R = G = B = pDoc->m_InputImage[i * pDoc->m_width + j];
+			pDC->SetPixel(j + 5, i + 5, RGB(R, G, B));
+		}
+	}
+
+	for (i = 0; i < pDoc->m_Re_height; i++) {
+		for (j = 0; j < pDoc->m_Re_width; j++) {
+			R = pDoc->m_OutputImage[i * pDoc->m_Re_width + j];
+			G = B = R;
+			pDC->SetPixel(j + pDoc->m_width + 10, i + 5, RGB(R, G, B));
+		}
+	}
+
+}
+
+
+// CMFCApplicationIPView 인쇄
+
+
+void CMFCApplicationIPView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMFCApplicationIPView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CMFCApplicationIPView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CMFCApplicationIPView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CMFCApplicationIPView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFCApplicationIPView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFCApplicationIPView 진단
+
+#ifdef _DEBUG
+void CMFCApplicationIPView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplicationIPView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplicationIPDoc* CMFCApplicationIPView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplicationIPDoc)));
+	return (CMFCApplicationIPDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplicationIPView 메시지 처리기
